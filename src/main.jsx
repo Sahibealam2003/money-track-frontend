d752089bf5782f7dce3beab7e03e44782bbc2684
Sahibealam2003 <alamsahib9719@gmail.com>
2026-02-06 19:58:57 +0530

    fix folder name case

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import UserProvider from "./Context/userContext.jsx";
+import UserProvider from "./context/userContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <UserProvider>
