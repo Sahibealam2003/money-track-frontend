e6e07dc116d0a482817a25915aac6d0541b72dec
Sahibealam2003 <alamsahib9719@gmail.com>
2026-02-06 20:11:34 +0530

    Fix

--- src/hooks/useUserAuth.jsx
+++ src/hooks/useUserAuth.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { UserContext } from "../Context/userContext";
+import { UserContext } from "../context/userContext";
 import { API_PATHS } from "../Utils/apiPath";
 import axiosInstance from "../Utils/axiosInstance";
 
